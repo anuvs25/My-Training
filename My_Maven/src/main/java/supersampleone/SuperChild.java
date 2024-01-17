906a383740e885516114bf6f310b5d589d4471f0
HP <HP@192.168.1.36>
2024-01-17 21:55:54 +0530

    Added some statements in child class

--- My_Maven/src/main/java/supersampleone/SuperChild.java
+++ My_Maven/src/main/java/supersampleone/SuperChild.java
@@ -2,6 +2,11 @@ package supersampleone;
 
 public class SuperChild extends SuperParent {
 	public void print() {
+//		if(a>b) 
+//		{System.out.println(a+" is greater");}
+//		else 
+//		{System.out.println(b+" is greater");}
+//		
 		System.out.println("This is super child");
 		System.out.println(super.a);
 		super.display();
